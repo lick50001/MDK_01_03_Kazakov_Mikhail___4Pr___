bc377bf5f9640a0fa65a0bbef8dcc8c29dc6001d
anekk <aneckalarionova@gmail.com>
2026-03-03 16:29:39 +0500

    Разработанный класс авторизации. Казаков

---
+++ app/src/main/java/com/example/a4pr/datas/apis/UserLogin.java
@@ -0,0 +1,45 @@
+package com.example.a4pr.datas.apis;
+
+import com.example.a4pr.datas.common.CheckInternet;
+import com.example.a4pr.domains.callbacks.MyResponseCallback;
+import com.example.a4pr.domains.callbacks.apis.MyAsyncTask;
+import com.google.gson.GsonBuilder;
+import com.example.a4pr.domains.models.User;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+public class UserLogin extends MyAsyncTask {
+    private User user;
+
+    public  UserLogin(User user, CheckInternet checkInternet, MyResponseCallback callback){
+        super(checkInternet, callback);
+        this.user = user;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids){
+        if (!checkInternet.isWiFiConnection() && !checkInternet.isMobileConnection())
+            return "Error : no internet connection";
+
+        String rawData = new GsonBuilder().create().toJson(this.user);
+
+        try {
+            Connection.Response response = Jsoup.connect("https://10.111.20.114:5000/swagger")
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.POST)
+                    .header("Content-type", "application/json")
+                    .requestBody(rawData)
+                    .execute();
+
+            return response.statusCode() == 200
+                    ? response.body()
+                    : "Error: " + response.body();
+        }catch (IOException e){
+            return "Error: " + e.getMessage();
+        }
+    }
+}
